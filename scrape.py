2aec72cd0623928096446cd94c4d1b11cad458c5
Raj Kondaveeti <46794263+rkOSU@users.noreply.github.com>
2021-01-13 00:10:47 -0500

    Modifying scraping
    Trying to implement scraping from a search and not just a single item page

--- scrape.py
+++ scrape.py
@@ -5,36 +5,76 @@ from bs4 import BeautifulSoup
 import smtplib
 
 # Website URL
-# TODO: ask user for link
+# Ask user for input
+print('Enter the URL for the item: ')
+URL = input()
 
-URL = 'https://www.amazon.com/Pet-Qwerks-BarkBone-Peanut-Butter/dp/B07PX3T3BF/ref=sr_1_19_sspa?dchild=1&keywords=dog+toy&qid=1600658055&sr=8-19-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyVkdGWjFCRjAxUVA4JmVuY3J5cHRlZElkPUEwODUyNDU2MUM2RzdBS0VUWTVURyZlbmNyeXB0ZWRBZElkPUEwMDM1OTYzMlZGMDM5VE9KT0lEViZ3aWRnZXROYW1lPXNwX210ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU='
+#URL = 'https://www.amazon.com/ROMOSS-Portable-Charger-Outputs-Compatible/dp/B07H5T9J4L/ref=sr_1_1_sspa?dchild=1&keywords=battery+pack&qid=1610427778&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExOUE4REdKVTdZTEJZJmVuY3J5cHRlZElkPUEwNDgyNjQ5MU9GOEIyUUo4M1cxTiZlbmNyeXB0ZWRBZElkPUEwOTE3NTkwODBZQzBDQUhNNTBOJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=='
 headers = {
-    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                  'Chrome/85.0.4183.102 Safari/537.36'}
+    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36', 'Cache-Control': 'no-cache', "Pragma": "no-cache"}
 
-title = ""
+
 
 def check_price():
     page = requests.get(URL, headers=headers)
     soup = BeautifulSoup(page.content, 'html.parser')
 
+    try:
+        # Scrape links of items
+        links = []
+        for item in soup.findAll("a", {"class":"a-size-base a-link-normal a-text-normal"}):
+            
+            links.append(item["href"])
+        
+        # Scrape prices of items
+        prices = []
+        for item in soup.findAll("span", {"class":"a-offscreen"}):
+            
+            prices.append(item.get_text().strip())
+        #print(prices)
 
-    # Extract tile of item
-    title_i = soup.find(id="productTitle")
-    print(title_i)
+        # Scrape names of items
+        names = []
+        for item in soup.findAll("span", {"class":"a-size-base-plus a-color-base a-text-normal"}):
+            
+            names.append(item.get_text().strip())
+        # Build item array
+        item_array = []
+        for i in range(0, len(names)):
+            item_info = []
+            item_info.append(prices[i])
+            item_info.append(names[i])
+            item_info.append(links[i])
+            item_array.append(item_info)
+            print(item_info)
 
-    title = soup.find(id="productTitle").get_text()
-    # Extract price of item
+        print(item_array)
+        # Sort list of items
+        item_array.sort()
 
-    price =  soup.find(id="priceblock_ourprice").get_text()
-    num_price = float(price[1:])
+        print(item_array[:3])
+        '''
+        #Scrape name of item
+        title = soup.find(id="productTitle").get_text().strip()
+        # Extract price of item
+        print(title)
+
+        price =  soup.find(id="priceblock_ourprice").get_text()
+        num_price = float(price[1:])
+        print (num_price)
 
 
-    if(num_price < 170):
-        send_email(title, price)
+        #if(num_price < 170):
+        #    send_email(title, price)
 
-    print(num_price)
-    print(title.strip())
+        #print(num_price)
+        #print(title.strip())
+        '''
+        return 1
+    except:
+        print("Error scraping!")
+        return -1
+    
 
 def send_email(product_name, price):
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -48,12 +88,13 @@ def send_email(product_name, price):
     body = 'Item: {}\nCurrent Price: {}\n\nLink: {}'.format(product_name, price, URL)
 
     msg = f"Subject: {subject}\n\n{body}"
-    server.sendmail('rajkondaveeti3@gmail.com',
-                    'boone.200@osu.edu',
-                    msg)
+    #server.sendmail('rajkondaveeti3@gmail.com',
+    #                'rajkondaveeti3@gmail.com',
+    #                msg)
     print("Email sent!")
 
     server.quit()
 
-while True:
-    check_price()
+flag = True
+while flag:
+    if check_price() == 1: flag = False
